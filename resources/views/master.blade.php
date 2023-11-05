b9936488bb36ae47b501f6cd7c0de5d223726464
Tuvsheee <Tuvshe1220@gmail.com>
2023-11-05 13:17:53 +0800

    up
    up

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> The Marina Hotel"</title>
+   
+    
+   
+    <link href="/css/style.css " rel="stylesheet">
+    
+
+</head>
+<body>
+        <h1>The Marina</h1>
+        <h2>WELCOME TO</h2>
+        <h3>Resort And Hotel</h3>
+       
+    <a class="booking" href="/page2">BOOK NOW</a> <!-- Link to navigate to Page 2 -->
+        
+</body>
+</html>
---
+++ resources/views/page3.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Promos and Offers"</title>
+   
+    
+   
+    <link href="/css/style1.css " rel="stylesheet">
+    
+
+</head>
+<body>
+    <div class="mhead" >Promos and Offers</div>
+    <div class="row">
+        <div class="column" width="100%">
+            <img src="/css/images/p1.png">
+        </div>
+        <div class="column" width="100%">
+            <img src="/css/images/p2.png">
+        </div>
+        <div class="column" width="100%">
+            <img src="/css/images/p3.png">
+        </div>
+    </div>
+    <a class="data4" href="/page2">
+        <div class="info2">Early Bird Discount </div>
+        <div class="info10">List your offers, promos, or special membership privileges and perks here to entice people to book your property.</div>
+    </div>
+    <a class="data5" href="/page2">
+        <div class="info5"> Wellhall Members Club</div>
+        <div class="info11">List your offers, promos, or special membership privileges and perks here to entice people to book your property.</div>
+    </div>
+    <a class="data6" href="/page2">
+        <div class="info8">Book 3 Nights,Get 1 Night Free</div>
+        <div class="info12">List your offers, promos, or special membership privileges and perks here to entice people to book your   property.</div>
+    </a>
+        
+            
+</body>
+</html>
